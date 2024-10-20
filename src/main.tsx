824ccebc0fba836b7e8d699a6d59b1b7a2c60bc0
Tom Middlehurst <25036538+tmiddlehurst@users.noreply.github.com>
2024-10-20 13:33:54 +0100

    axios base path

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import axios from 'axios';
+import { BASE_PATH } from './types/generated/base.ts';
 
 async function enableMocking() {
   if (process.env.NODE_ENV !== 'development') {
+    axios.defaults.baseURL = BASE_PATH;
     return;
   }
   const { worker } = await import('./mock/browser');
