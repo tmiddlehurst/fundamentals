50ff6d9aef4232529f6e7b92db8caad55217b970
Tom Middlehurst <25036538+tmiddlehurst@users.noreply.github.com>
2024-10-28 23:14:55 +0000

    subscribe to ticker

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,40 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { Stock } from './types/types';
+import StockList from './components/StockList';
 
 function App() {
-  console.log('app rendered');
+  const [symbol, setSymbol] = useState('');
+  // const [stocks, setStocks] = useState<Stock[]>([]);
 
-  const [count, setCount] = useState(0);
+  const socketRef = useRef<WebSocket>();
+
+  useEffect(() => {
+    const socket = new WebSocket('ws://localhost:8080/ws/mock-data');
+    socketRef.current = socket;
+
+    socket.onmessage = (event) => {
+      console.log(event.data);
+    };
+
+    return () => socket.close();
+  }, []);
+
+  function subscribe() {
+    socketRef.current?.send(`subscribe:${symbol}`);
+  }
+
+  // useEffect(() => {
+  //   axios.get('/stocks').then((res: AxiosResponse<Stock[]>) => {
+  //     setStocks(res.data);
+  //   });
+  // }, []);
 
   return (
     <>
       <h1>Fundamentals</h1>
+      <input onChange={(e) => setSymbol(e.target.value)} />
+      <button onClick={subscribe}>Subscribe</button>
+      <StockList stocks={stocks} />
     </>
   );
 }
