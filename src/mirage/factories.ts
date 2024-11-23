2c4e493a947dde01c7986ebca9871807c0062ac1
Tom Middlehurst <25036538+tmiddlehurst@users.noreply.github.com>
2024-11-23 12:26:20 +0000

    randomise note factory

--- src/mirage/factories.ts
+++ src/mirage/factories.ts
@@ -11,20 +11,32 @@ const noteFactory = Factory.extend({
   id(i) {
     return `${i}`;
   },
-  name: 'Apple',
-  symbol: 'AAPL',
-  title: 'Analysis of Apple on 12/12/24',
-  date: '2022-10-01T22:43:41.566Z',
-  price: 908901583159296,
+  name(i) {
+    return examples[i % (examples.length - 1)][1];
+  },
+  symbol(i) {
+    return examples[i % (examples.length - 1)][0];
+  },
+  title(i) {
+    const r = Math.random() * 365 * (1000 * 3600 * 24);
+    const now = Date.now();
+    const date = new Date(now - r);
+    const company = examples[i % (examples.length - 1)];
+    return `Analysis of ${company} on ${date.toDateString()}`;
+  },
+  date() {
+    const r = Math.random() * 365 * (1000 * 3600 * 24);
+    const now = Date.now();
+    const date = new Date(now - r);
+    return date.toISOString();
+  },
+  price: 210,
   note: 'string',
-  priceTarget: 635834900414464,
-  followUpDate: '2028-10-27T07:46:19.663Z',
-  confidence: Math.ceil(Math.random() * 10),
-  afterCreate(note) {
-    let n = Math.floor(Math.random() * examples.length);
-    note.symbol = examples[n][0];
-    note.name = examples[n][1];
-  }
+  priceTarget: 190,
+  followUpDate: '2024-10-27T07:46:19.663Z',
+  confidence() {
+    return Math.ceil(Math.random() * 10);
+  },
 });
 
 export { noteFactory };
