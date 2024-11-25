c0397efa695509709150071ee3cd3f433026055a
Tom Middlehurst <25036538+tmiddlehurst@users.noreply.github.com>
2024-11-25 22:24:21 +0000

    candle chart

--- src/components/WatchedStockDetail.tsx
+++ src/components/WatchedStockDetail.tsx
@@ -1,5 +1,62 @@
-import { WatchedStock } from '../types/types';
+import { useContext, useEffect, useState } from 'react';
+import { ChartInterval, WatchedStock } from '../types/types';
+import { AxiosContext } from '../context/AxiosAPIContext';
+import Chart from 'react-apexcharts';
+import { TimeSeriesPoint } from '../types/generated';
+
+type ApexChartCandlestickChartPoint = [
+  number,
+  [number, number, number, number]
+];
+
+function toApexCandlestickPoint(
+  point: TimeSeriesPoint
+): ApexChartCandlestickChartPoint {
+  return [
+    new Date(point.datetime).valueOf(),
+    [+point.open, +point.high, +point.low, +point.close],
+  ];
+}
 
 export default function WatchedStockDetail({ stock }: { stock: WatchedStock }) {
-  return <div className='flex-1 rounded-md bg-white'>{stock.name}</div>;
+  const api = useContext(AxiosContext);
+  const [chartInterval, setChartInterval] = useState<ChartInterval>('5min');
+  const [chartSeries, setChartSeries] =
+    useState<ApexChartCandlestickChartPoint[]>();
+
+  useEffect(() => {
+    api
+      .getTimeSeriesData(stock.symbol, 'TOMS_API_KEY', chartInterval)
+      .then((data) => {
+        const series = data.values?.map((val) => toApexCandlestickPoint(val));
+        setChartSeries(series);
+      });
+  }, [stock, chartInterval]);
+  const chartConfig = {
+    series: [
+      {
+        data: chartSeries,
+      },
+    ],
+    options: {
+      chart: {
+        id: `${stock.symbol}-candle-chart`,
+      },
+    },
+  };
+  return (
+    <div className='flex-1 rounded-md bg-white flex flex-col justify-between'>
+      {stock.name}
+      <div>
+        {chartSeries?.length && (
+          <Chart
+            series={chartConfig.series}
+            options={chartConfig.options}
+            type='candlestick'
+          />
+        )}
+      </div>
+      <div>notes go here</div>
+    </div>
+  );
 }
--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -17,3 +17,4 @@ export type Ticker = string;
 
 export const symbols = ['AAPL', 'MSFT', 'AMZN'] as const;
 export type Symbol = (typeof symbols)[number];
+export type ChartInterval = '1min' | '5min' | '15min' | '30min' | '45min' | '1h' | '2h' | '4h' | '1day' | '1week' | '1month';
